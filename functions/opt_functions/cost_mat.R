c94d293975a192c9c29499f796274dec541ec0bf
Anthony-Kwong <94846235+Anthony-Kwong@users.noreply.github.com>
2024-03-04 11:13:26 +0000

    Update cost_mat.R

--- functions/opt_functions/cost_mat.R
+++ functions/opt_functions/cost_mat.R
@@ -12,8 +12,56 @@
 #'
 #' @examples
 cost_mat <- function(p,q, k = 2){
+  #get vectors for source distribution
   
+  #frequency
+  f = p$freq
+  #same sampling rate for every recording so these should be equal
+  testthat::expect_equal(p$freq, q$freq)
+  
+  #time
+  tp = p$time
+  tq = q$time
+  
+  #get f and t values as tables ----
+  vals = lapply(list(tp,tq), function(t){
+    #index vectors to keep track of which elements to use
+    p_reps = length(t)
+    pf_index = rep(length(f):1, p_reps)
+    pt_index = rep(seq(p_reps), each = length(f))
+    #use indices to fetch the frequencies and times to use for computing costs
+    pf = sapply(pf_index, function(x){f[x]})
+    pt = sapply(pt_index, function(y){t[y]})
+    tibble::tibble(freq.ticks = pf,time.ticks =pt)
+  })
+  
+  p_vals = vals[[1]]
+  q_vals = vals[[2]]
+  
+  #check correct dimensions
+  testthat::expect_equal(nrow(p_vals), length(f)*length(p$time))
+  testthat::expect_equal(nrow(q_vals), length(f)*length(q$time))
+  
+  #Compute matrix ----
+  plen = nrow(p_vals)
+  qlen = nrow(q_vals)
+  
+  C = matrix(0, ncol = plen, nrow = qlen)
+  #loop over rows
+  for(i in 1:qlen){
+    #loop over cols
+    for(j in 1:plen){
+      Q = q_vals[i,]
+      P = p_vals[j,]
+      C[i,j] = (Q$freq.ticks - P$freq.ticks)^k + a^(k)*(Q$time.ticks - P$time.ticks)^k
+    }
+  }
+  
+  return(C)
 }
+
+#test
+
 
 # cost.mat.k <- 2 # k=2 is the Wasserstein metric
 # mat.tmp <- rep( 0.0, length(p.mid.pts)*length(q.mid.pts) )
